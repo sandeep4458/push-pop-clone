1fb8fcd596a14f73a1a1382290a50f03ea7ea4e0
sandeep sunny <84172477+sandeep4458@users.noreply.github.com>
2022-12-12 12:44:21 +0530

    headings added

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,12 @@ function App() {
        
         <h1>This heading is created by sandeep</h1>
         <h1>this is sunny</h1>
+    <h3>sunny sunny sunny</h3>
+        <h3>sunny sunny sunny</h3>
+    <h3>sunny sunny sunny</h3>
+
+        <h3>sunny sunny sunny</h3>
+
     </div>
   );
 }
